26830ba9242c6532ce9ca7b83099e804ec717cb9
Pavel1G <94784915+Pavel1G@users.noreply.github.com>
2023-03-14 23:35:52 +0300

    CourseWork are done

---
+++ src/main/java/pro/sky/telegrambot/timer/NotificationTaskTimer.java
@@ -0,0 +1,32 @@
+package pro.sky.telegrambot.timer;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pro.sky.telegrambot.service.NotificationTaskService;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class NotificationTaskTimer {
+
+    private final NotificationTaskService notificationTaskService;
+    private final TelegramBot telegramBot;
+
+    public NotificationTaskTimer(NotificationTaskService notificationTaskService, TelegramBot telegramBot) {
+        this.notificationTaskService = notificationTaskService;
+        this.telegramBot = telegramBot;
+    }
+
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    public void checkNotification() {
+        notificationTaskService.findNotificationTaskForSend().forEach(notificationTask -> {
+            telegramBot.execute(new SendMessage(notificationTask.getUserId(),
+                    "Напоминаю: " + notificationTask.getMessage())
+            );
+            notificationTaskService.deleteTask(notificationTask);
+        });
+    }
+}
